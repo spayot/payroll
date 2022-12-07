e3d18c45f7fe8bbcd3979d51ea9385ffd4c750f1
Sylvain Payot <sylvain.payot@nike.com>
2022-12-06 22:44:42 -0800

    add example.yml file

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from collections import namedtuple
-
-import yaml
+import argparse
 
 from src import payroll as pr
 
-FILEPATH = "data/services.yml"
+DEFAULT_FILEPATH = "data/services.yml"
 
 
 def main(filepath):
     registry = pr.ServiceTypeRegistry.from_yaml(filepath)
 
     current_week_dates = pr.generate_current_week_dates()
+
     gui = pr.PayPeriodGUI(
         dates=current_week_dates,
         registry=registry,
@@ -21,4 +20,15 @@ def main(filepath):
 
 
 if __name__ == "__main__":
-    main(FILEPATH)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--path",
+        default="data/example.yml",
+        help="path to the services config YAML file",
+        type=str,
+    )
+
+    args = parser.parse_args()
+
+    main(args.path)
